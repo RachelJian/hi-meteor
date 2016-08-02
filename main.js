b51b7d7c731fbb10380a814d61af7b8a4d04f242
rachelchien <r821103542@gmail.com>
2016-08-02 11:27:38 +0800

    sort message

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-console.log("this is both sides");
+// console.log("this is both sides");
 Message = new Mongo.Collection("message");
 
-sampleMessages = [
-	{text:"hi text1 (m1)",from:"db"},
-	{text:"hi text2 (m2)",from:"db"},
-	{text:"hi text3 (m3)",from:"db"},
-	{text:"hi text4 (m4)",from:"db"}
-	 ]
+// sampleMessages = [
+// 	{text:"hi text1 (m1)",from:"db"},
+// 	{text:"hi text2 (m2)",from:"db"},
+// 	{text:"hi text3 (m3)",from:"db"},
+// 	{text:"hi text4 (m4)",from:"db"}
+// 	 ]
 
 
 if (Meteor.isClient){
-	console.log("this is client");
-	
+	// console.log("this is client");
 
 	Template.body.helpers({
 		msg: function(){
-			return(Message.find())
-
+			return(Message.find({},{sort:{createdAt: -1}}))
 
 		}
 		
@@ -31,34 +29,32 @@ if (Meteor.isClient){
 		Template.body.events({
 			"change #inputMsg": function(e,t){
 
-				console.log("change #inputMsg");
-				console.log($(e.target).val());
+				// console.log("change #inputMsg");
+				// console.log($(e.target).val());
+				
 				msg = $(e.target).val();
 				$(e.target).val("");
 				msgData = {
 					text:msg,
-					createAt: new Date,
+					createdAt: new Date,
 				};
-				console.log(msgData);
+				// console.log(msgData);
 				Message.insert(msgData);
-
-				
-
 
 			}
 		})
 
 }
 
-if (Meteor.isServer){
- 	console.log("this is server");
- 	if (Message.find().count() === 0){
-		for (i in sampleMessages){
-		  Message.insert(sampleMessages[i]);
-		} 
-	}
+// if (Meteor.isServer){
+//  	console.log("this is server");
+//  	if (Message.find().count() === 0){
+// 		for (i in sampleMessages){
+// 		  Message.insert(sampleMessages[i]);
+// 		} 
+// 	}
 
-}
+// }
 
 
 
