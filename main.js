9df29376dea3d2fbd9781668181d701cf64d5a41
rachelchien <r821103542@gmail.com>
2016-11-07 14:50:43 +0800

    change title

--- main.js
+++ main.js
@@ -32,17 +32,22 @@ if (Meteor.isClient){
 				msg = $(e.target).val();
 				usr = $("#inputUsr").val();
 
-				if (!usr) {
-					usr = "Anonymous";
-				}
+				
 
 				$("form > input").val("");
 				
 				msgData = {
 					text:msg,
-					user:usr,
 					createdAt: new Date,
 				};
+
+				usr = Meteor.userId()
+				if (usr) {
+					msgData.userId = usr;
+				    msgData.user = Meteor.user().profile.name;
+
+				}
+
 				Message.insert(msgData);
 
 			}
